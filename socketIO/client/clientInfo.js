622e16a99ff5f5b7b161fb05bf1ec2345c44d340
rafael najjar <rafaelnajjar@gmail.com>
2023-03-05 02:11:11 -0300

    created getUserInfo request

--- socketIO/client/clientInfo.js
+++ socketIO/client/clientInfo.js
@@ -1,4 +1,8 @@
 const clientInfo = (io, client, info) => {
+    client.on('getClientInfo', () => {
+        const userInfo = info.loggedUsers.find(user => user.id === client.id)
+        client.emit("ReceiveClientInfo", userInfo)
+    })
 }
 
 module.exports = {clientInfo};
