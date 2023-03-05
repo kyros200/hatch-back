026bebe33db8bdb515e3e811d78bfd4f19cd1d3d
rafael najjar <rafaelnajjar@gmail.com>
2023-03-05 01:45:26 -0300

    minor fix on chat about channel

--- socketIO/projects/chat/chat.js
+++ socketIO/projects/chat/chat.js
@@ -1,16 +1,17 @@
 const chat = (io, client, info) => {
-    client.on('sendChat', ({message, room}) => {
+    client.on('sendChat', ({message, channel , toUser}) => {
         const userInfo = info.loggedUsers.find(user => user.id === client.id)
 
         console.log(`${userInfo.user}: ${message}`)
 
         let path = io
 
-        if(userInfo.room) path = path.to(userInfo.room)
+        if(channel === "room") path = path.to(userInfo.room)
+        if(channel === "user") path = path.to(toUser)
 
         path.emit("receiveChat", {
             message: `${userInfo.user}: ${message}`,
-            channel: room
+            channel: channel
         })
     })
 }
