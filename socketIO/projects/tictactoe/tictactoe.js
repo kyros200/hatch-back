892bf248adb869978ab3dd3c71984ffd1398cbb2
unknown <rafaelnajjar@gmail.com>
2023-11-13 02:10:58 -0300

    tictactoe v1 finished

--- socketIO/projects/tictactoe/tictactoe.js
+++ socketIO/projects/tictactoe/tictactoe.js
@@ -28,25 +28,35 @@ const tictactoe = (io, client, info) => {
 
         let newRoomIndex = info[choosenProject].rooms.map((room) => room.name).indexOf(roomName)
 
-        const newEvent = `${roomName} turn ${info[choosenProject].rooms[newRoomIndex].matchInfo.events.length + 1}: ${user.user} choosed row ${cell[0]} column ${cell[1]}`
+        const gameRoom = info[choosenProject].rooms[newRoomIndex]
+
+        gameRoom.matchInfo.board[cell[0]][cell[1]] = gameRoom.matchInfo.symbol
+
+        const isFinished = checkVictory(gameRoom.matchInfo)
+
+        let newEvent = `${roomName} turn ${gameRoom.matchInfo.events.length + 1}: ${user.user} choosed row ${cell[0] + 1} column ${cell[1] + 1}.`
         
-        info[choosenProject].rooms[newRoomIndex].matchInfo.board[cell[0]][cell[1]] = info[choosenProject].rooms[newRoomIndex].matchInfo.symbol
-        info[choosenProject].rooms[newRoomIndex].matchInfo.events.push({user, cell, roomName, message: newEvent})
-        const isFinished = checkVictory(info[choosenProject].rooms[newRoomIndex].matchInfo)
+        if(isFinished === "T") {
+            newEvent += " It's a Tie!"
+        }
 
         if(isFinished === "O" || isFinished === "X") {
-            info[choosenProject].rooms[newRoomIndex].matchInfo.winner = user
-            info[choosenProject].rooms[newRoomIndex].matchInfo.events.push({winner: user.user, message: `${user.user} won!!!`})
-            info[choosenProject].rooms[newRoomIndex].matchStatus = "ENDED"
+            newEvent += ` ${isFinished} won!`
+        }
+
+        gameRoom.matchInfo.events.push({user, cell, roomName, message: newEvent})
+
+        if(isFinished === "O" || isFinished === "X") {
+            gameRoom.matchInfo.winner = user
+            gameRoom.matchStatus = "ENDED"
             console.log(`${choosenProject} match finished in room ${roomName}`)
         } else if (isFinished === "T") {
-            info[choosenProject].rooms[newRoomIndex].matchInfo.winner = {}
-            info[choosenProject].rooms[newRoomIndex].matchInfo.events.push({winner: "", message: `${user.user} tied the match!!!`})
-            info[choosenProject].rooms[newRoomIndex].matchStatus = "ENDED"
+            gameRoom.matchInfo.winner = {}
+            gameRoom.matchStatus = "ENDED"
             console.log(`${choosenProject} match finished in room ${roomName}`)
         } else {
-            info[choosenProject].rooms[newRoomIndex].matchInfo.turn = info[choosenProject].rooms[newRoomIndex].matchInfo.turn === 0 ? 1 : 0
-            info[choosenProject].rooms[newRoomIndex].matchInfo.symbol = info[choosenProject].rooms[newRoomIndex].matchInfo.symbol === "O" ? "X" : "O"
+            gameRoom.matchInfo.turn = gameRoom.matchInfo.turn === 0 ? 1 : 0
+            gameRoom.matchInfo.symbol = gameRoom.matchInfo.symbol === "O" ? "X" : "O"
         }
 
         //each action made we need to update every player
